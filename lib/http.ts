7e1a5f4442c012e0625d7fc6ce5ae42893a76916
Bhogeshwar Jadhav <bhogeshwarj@gmail.com>
2025-03-30 18:27:36 +0530

    add:authentication token

--- lib/http.ts
+++ lib/http.ts
@@ -1,7 +1,23 @@
 import axios from "axios";
+import * as SecureStore from "expo-secure-store";
 
 const http = axios.create({
   baseURL: "https://newzy.ca/api",
 });
+
+http.interceptors.request.use(
+  async (config) => {
+    try {
+      const token = await SecureStore.getItemAsync("authToken");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    } catch (error) {
+      console.error("Error retrieving token:", error);
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
 
 export { http };
